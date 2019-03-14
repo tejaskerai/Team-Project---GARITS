7444dd925e5734c3dedb2b9fcbcc71bb5a235af7
Tadas <tadas.vaisvila.1996@gmail.com>
2019-03-14 11:18:13 +0000

    DBImplementation.java half done

--- src/Database/DBConnectivity.java
+++ src/Database/DBConnectivity.java
@@ -1,35 +1,17 @@
 package Database;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+
 
 public interface DBConnectivity {
 
 
 
-	abstract void read();
+	 void read();
 
-	abstract void write();
+	void write();
 
-	public static void main(String a[]){
+	 void connect();
 
-		try {
-			Class.forName("oracle.jdbc.driver.OracleDriver");
-			Connection con = DriverManager.
-					getConnection("jdbc:oracle:thin:@<hostname>:<port num>:<DB name>"
-							,"user","password");
-			Statement stmt = con.createStatement();
-			System.out.println("Created DB Connection....");
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-	abstract void disconnect();
+	 void disconnect();
 
 }
--- src/Database/DBImplementation.java
+++ src/Database/DBImplementation.java
@@ -1,41 +1,88 @@
-//package Database;
-//
-//import Database.DBConnectivity.*;
-//
-//public class DBImplementation implements write {
-//
-//	public connection connect() {
-//		// TODO - implement DBImplementation.connect
-//		throw new UnsupportedOperationException();
-//	}
-//
-//	/**
-//	 * 
-//	 * @param SQL
-//	 * @param conn
-//	 */
-//	public resultSet read(string SQL, connection conn) {
-//		// TODO - implement DBImplementation.read
-//		throw new UnsupportedOperationException();
-//	}
-//
-//	/**
-//	 * 
-//	 * @param SQL
-//	 * @param conn
-//	 */
-//	public boolean write(string SQL, connection conn) {
-//		// TODO - implement DBImplementation.write
-//		throw new UnsupportedOperationException();
-//	}
-//
-//	/**
-//	 * 
-//	 * @param conn
-//	 */
-//	public boolean disconnect(connection conn) {
-//		// TODO - implement DBImplementation.disconnect
-//		throw new UnsupportedOperationException();
-//	}
-//
-//}
+package Database;
+
+import Database.DBConnectivity.*;
+
+import java.sql.*;
+
+import java.sql.SQLException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+
+public class DBImplementation implements DBConnectivity {
+
+    private Connection con = null;
+    private PreparedStatement ps = null;
+    private ResultSet rs = null;
+
+    private String userName;
+    private String password;
+    private String SQL;
+
+    @Override
+    public void read() {
+
+    }
+
+    @Override
+    public void write() {
+
+    }
+
+    public  void connect() {
+        try {
+            // db parameters
+            String url = "jdbc:sqlite:C:\\GARITS_system\\src\\Database\\GARITSdb.db";
+            // create a connection to the database
+            con = DriverManager.getConnection(url);
+
+            System.out.println("Connection to SQLite has been established.");
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            try {
+                if (con != null) {
+                    con.close();
+                }
+            } catch (SQLException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+    }
+
+
+
+    @Override
+    public void disconnect() {
+
+        try { rs.close(); } catch (Exception e) { /* ignored */ }
+        try { ps.close(); } catch (Exception e) { /* ignored */ }
+        try { con.close(); } catch (Exception e) { /* ignored */ }
+
+    }
+
+
+    /**
+     *
+     * @param SQL SELECT column or table
+     * @param  con !null
+     */
+    public ResultSet read(String SQL, Connection con) {
+        // TODO - implement DBImplementation.read
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     *
+     * @param SQL SELECT and UPDATE column or table
+     * @param conn !null
+     */
+    public boolean write(String SQL, Connection conn) {
+        // TODO - implement DBImplementation.write
+        throw new UnsupportedOperationException();
+    }
+
+
+
+}
