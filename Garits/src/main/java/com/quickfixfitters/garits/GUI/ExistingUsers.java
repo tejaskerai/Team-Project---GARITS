17f392686e40fc65cc766d95e21bd289295e20e6
Tejas Kerai <tejas_444@hotmail.co.uk>
2019-03-22 21:22:44 +0000

    editable table fix

--- Garits/src/main/java/com/quickfixfitters/garits/GUI/ExistingUsers.java
+++ Garits/src/main/java/com/quickfixfitters/garits/GUI/ExistingUsers.java
@@ -88,7 +88,7 @@ public class ExistingUsers extends javax.swing.JFrame {
             }
         ) {
             boolean[] canEdit = new boolean [] {
-                false, false, true, false
+                false, false, false, false
             };
 
             public boolean isCellEditable(int rowIndex, int columnIndex) {
