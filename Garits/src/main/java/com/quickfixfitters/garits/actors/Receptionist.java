e5bf44c02949689f71d4c8578225b99d1d7e727e
Conor Crampsie <c.crampsie@hotmail.co.uk>
2019-04-01 17:35:22 +0100

    MoT Reminder files now have content

--- Garits/src/main/java/com/quickfixfitters/garits/actors/Receptionist.java
+++ Garits/src/main/java/com/quickfixfitters/garits/actors/Receptionist.java
@@ -1,6 +1,7 @@
 package com.quickfixfitters.garits.actors;
 
 import com.quickfixfitters.garits.database.DBConnectivity;
+import com.quickfixfitters.garits.entities.Customer;
 import com.quickfixfitters.garits.entities.MOTReminder;
 import java.io.File;
 import java.io.IOException;
@@ -44,19 +45,52 @@ public class Receptionist extends User {
             File newReminder = new File(fileName);
             newReminder.createNewFile();
             
-            ArrayList<String> infoToWrite = new ArrayList<String>();
-            
-            
             PrintWriter writer = new PrintWriter(fileName, "UTF-8");
-            for (String info : infoToWrite){
-                writer.println(info);
-            }
+            
+            printOurAddress(writer);
+            printTheirAddress(writer, reminder, session);
+            
+            writer.println(dateFormat.format(new Date()));
+            writer.println("");
+            writer.println("Dear " + reminder.getMotVehicle().getCustomer().getForename() + " " +
+                    reminder.getMotVehicle().getCustomer().getSurname() + ",");
+            writer.println("");
+            
+            writer.println("REMINDER - MoT TEST DUE");
+            writer.println("Vehicle Registration No: " + reminder.getMotVehicle().getRegNo() + 
+                    "Renewal Test Date: " + dateFormat.format(reminder.getMotVehicle().getRenewalTestDate()));
+            writer.println("");
+            
+            writer.println("The above vehicle is due to have its MoT certificate renewed on the date shown.");
+            writer.println("");
+            writer.println("We hope you will use our services to carry out the necessary test.");
+            writer.println("");
+            
+            writer.println("Yours sincerely,");
+            writer.println("G. Lancaster");
+            
             writer.close();
             JOptionPane.showMessageDialog(null, "File created");
         }catch(IOException e){
             JOptionPane.showMessageDialog(null, "Printing failed");
             reminder.setPrinted(0);
         }
+    }
+    
+    public void printTheirAddress(PrintWriter writer, MOTReminder reminder, Session session){
+        writer.println(reminder.getMotVehicle().getCustomer().getForename().charAt(0) + 
+                ". " + reminder.getMotVehicle().getCustomer().getSurname() + ",");
+        writer.println(reminder.getMotVehicle().getCustomer().getCustomerAddress() + ",");
+        writer.println(reminder.getMotVehicle().getCustomer().getPostcode());
+        writer.println("");
+    }
+    
+    public void printOurAddress(PrintWriter writer){
+        writer.println("Quick Fix Fitters.,");
+        writer.println("19 High St.,");
+        writer.println("Ashford,");
+        writer.println("Kent CT16 8YY");
+        writer.println("");
     }
 
     public String getUsername() {
