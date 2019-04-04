ea073936a4a3ab0458d8d2f50daf9e7b4592541c
Tadas <tadas.vaisvila.1996@gmail.com>
2019-04-04 12:28:16 +0100

    updated comments

--- Garits/src/main/java/com/quickfixfitters/garits/factories/EmployeeFactory.java
+++ Garits/src/main/java/com/quickfixfitters/garits/factories/EmployeeFactory.java
@@ -6,6 +6,9 @@ import java.util.List;
 
 public class EmployeeFactory implements FactoryInterface<Employee> {
     @Override
+    /*
+    * creates default Admin
+    * */
     public Employee Produce() {
         return new Employee(
                 "test", "user", "admin", "test", "test123"
