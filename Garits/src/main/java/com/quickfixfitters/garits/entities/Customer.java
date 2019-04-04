d165ecb688aa37783baa83e496a259ad60590c75
Tadas <tadas.vaisvila.1996@gmail.com>
2019-04-04 12:16:56 +0100

    updated comments

--- Garits/src/main/java/com/quickfixfitters/garits/entities/Customer.java
+++ Garits/src/main/java/com/quickfixfitters/garits/entities/Customer.java
@@ -48,78 +48,123 @@ public class Customer {
         this.telNo = telNo;
         this.email = email;
     }
-
+/*
+* Hibernate constructor
+* */
     public Customer() {
     }
 
+    /*
+     * Gets CustomerID
+     * */
     public int getCustomerID() {
         return customerID;
     }
 
+    /*
+     * Sets CustomerID
+     * */
     public void setCustomerID(int customerID) {
         this.customerID = customerID;
     }
 
+    /*
+     * Gets Forename
+     * */
     public String getForename() {
         return forename;
     }
 
+    /*
+     * Sets Forename
+     * */
     public void setForename(String forename) {
         this.forename = forename;
     }
 
+    /*
+    * Gets Surname
+    * */
     public String getSurname() {
         return surname;
     }
 
+    /*
+     * Sets Surname
+     * */
     public void setSurname(String surname) {
         this.surname = surname;
     }
-
+    /*
+     * Gets Customer Address
+     * */
     public String getCustomerAddress() {
         return customerAddress;
     }
-
+    /*
+     * Sets Customer Address
+     * */
     public void setCustomerAddress(String customerAddress) {
         this.customerAddress = customerAddress;
     }
-
+    /*
+     * Gets Telephone number
+     * */
     public String getTelNo() {
         return telNo;
     }
-
+    /*
+     * Sets Telephone number
+     * */
     public void setTelNo(String telNo) {
         this.telNo = telNo;
     }
 
+    /*
+    *  Gets Email
+    * */
     public String getEmail() {
         return email;
     }
-
+    /*
+     * Sets Email
+     * */
     public void setEmail(String email) {
         this.email = email;
     }
-
+    /*
+    *  Gets Postcode
+    * */
     public String getPostcode() {
         return postcode;
     }
-
+    /*
+     * Sets Postcode
+     * */
     public void setPostcode(String postcode) {
         this.postcode = postcode;
     }
-    
+    /*
+    * Gets Customer Account
+    * */
     public CustomerAccount getCustomerAccount() {
         return customerAccount;
     }
-
+    /*
+    * Sets Customer Account
+    * */
     public void setCustomerAccount(CustomerAccount customerAccount) {
         this.customerAccount = customerAccount;
     }
-
+    /*
+    *  Gets Vehicles registered under this Customer
+    * */
     public List<Vehicle> getVehicles() {
         return vehicles;
     }
-
+    /*
+    *  Sets Vehicles registered under this Customer
+    * */
     public void setVehicles(List<Vehicle> vehicles) {
         this.vehicles = vehicles;
     }
